666c50143f893ce0e818098c56d7bca7cf34c662
Andrii <user@Andrii-Melnuk.local>
2025-07-18 23:19:28 +0300

    not ended commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './page/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Home />
     </div>
   );
 }
