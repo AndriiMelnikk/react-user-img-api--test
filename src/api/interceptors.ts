e71ba4967d1311b10259034d15692dea92d482a5
Andrii <user@Andrii-Melnuk.local>
2025-07-21 02:17:08 +0300

    setings local run

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "https://6b0c329c-acfe-4543-8454-c9f518e81aed-00-10it4wmkw0pj0.janeway.replit.dev:3000/";
+  "http://localhost:8080/";
 
 const Service = axios.create({
   baseURL: API_URL,
